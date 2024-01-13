8ede7329a97b50ca58bc796038e067ba8bb3b377
Tanishq Parkar <78678620+RevTpark@users.noreply.github.com>
2024-01-14 01:10:29 +0530

    base logic in place

--- index.ts
+++ index.ts
@@ -20,67 +20,63 @@ const dbConfig = {
 
 const getEmoji = (name: string) => {
     const skill = skillsMap.find(x => x.name === name)
-    if(skill) return skill.emoji
+    if (skill) return skill.emoji
     return '🤖'
 }
 
 const getRoleFromSkill = (name: string) => {
     const skill = skillsMap.find(x => x.name === name)
-    if(skill) return skill.role
+    if (skill) return skill.role
 }
 
 client.once('ready', async () => {
-
     console.log(`Logged in as ${client.user?.tag}`);
 
     // const cronTime = '*/5 * * * * *'
-    // const cronTime = '0 */12 * * *'
-    // cron.schedule(cronTime, async () => {
-    //     console.log('Running cron job...');
-    // })
+    const cronTime = '0 */12 * * *'
+    cron.schedule(cronTime, async () => {
+        const connection = await mysql.createConnection(dbConfig);
+        const [rows] = await connection.execute(`SELECT * FROM Bounties WHERE isPublished=1 AND isActive=1 AND hackathonprize=0 AND isArchived=0 AND status='OPEN' AND createdAt BETWEEN NOW() - INTERVAL 12 HOUR AND NOW()`);
+        const bounties: Bounties[] = rows as Bounties[];
 
-    const connection = await mysql.createConnection(dbConfig);
-    // TODO: createdAt BETWEEN NOW() - INTERVAL 12 HOUR AND NOW()
-    const [rows] = await connection.execute(`SELECT * FROM Bounties WHERE isPublished=1 AND isActive=1 AND hackathonprize=0 AND isArchived=0 AND status='OPEN'`);
-    const bounties: Bounties[] = rows as Bounties[];
+        let roles: Set<string> = new Set();
+        let bountyMessage = ''
 
-    let roles: Set<string> = new Set();
-    let bountyMessage = ''
-    
-    if (bounties.length === 1) {
-        const emoji = getEmoji(bounties[0].skills[0].skills)
-        bounties[0].skills.forEach(sk => roles.add(getRoleFromSkill(sk.skills)))
+        if (bounties.length === 1) {
+            const emoji = getEmoji(bounties[0].skills[0].skills)
+            bounties[0].skills.forEach(sk => roles.add(getRoleFromSkill(sk.skills)))
 
-        bountyMessage += `${emoji} ${bounties[0].title} (\$${bounties[0].rewardAmount})\n\n🔗 https://earn.superteam.fun/listings/bounties/${bounties[0].slug}/?utm_source=superteam&utm_medium=discord&utm_campaign=bounties\n\n`
-    }
-    else {
-        bountyMessage += `🚨 New Bounties Listing!\n\n`
-        bounties.slice(1, 3).forEach(x => {
-            const emoji = getEmoji(x.skills[0].skills)
-            x.skills.forEach(sk => roles.add(getRoleFromSkill(sk.skills)))
-            bountyMessage += `${emoji} ${x.title} (\$${x.rewardAmount})\n\n🔗 <https://earn.superteam.fun/listings/bounties/${x.slug}/?utm_source=superteam&utm_medium=discord&utm_campaign=bounties>\n\n`
-        })
-    }
-
-    const rolesArray = Array.from(roles)
-
-    servers.map((server) => {
-        const guild = client.guilds.cache.get(server.id)
-        if (guild) {
-            server.coreRoles.forEach((role) => {
-                bountyMessage += `${role.id} `
-            })
-            rolesArray.forEach((role) => {
-                const guildRole = server.roles.find(x => x.name === role)
-                if (guildRole) {
-                    bountyMessage += `${guildRole.id} `
-                }
-            })
-            const channel = guild.channels.cache.get(server.earn)
-            if (channel && channel.isTextBased()) {
-                channel.send(bountyMessage)
-            }
+            bountyMessage += `${emoji} ${bounties[0].title} (\$${bounties[0].rewardAmount})\n\n🔗 https://earn.superteam.fun/listings/bounties/${bounties[0].slug}/?utm_source=superteam&utm_medium=discord&utm_campaign=bounties\n\n`
         }
+        else {
+            bountyMessage += `🚨 New Bounties Listing!\n\n`
+            bounties.forEach(x => {
+                const emoji = getEmoji(x.skills[0].skills)
+                x.skills.forEach(sk => roles.add(getRoleFromSkill(sk.skills)))
+                bountyMessage += `${emoji} ${x.title} (\$${x.rewardAmount})\n\n🔗 <https://earn.superteam.fun/listings/bounties/${x.slug}/?utm_source=superteam&utm_medium=discord&utm_campaign=bounties>\n\n`
+            })
+        }
+
+        const rolesArray = Array.from(roles)
+
+        servers.map((server) => {
+            const guild = client.guilds.cache.get(server.id)
+            if (guild) {
+                server.coreRoles.forEach((role) => {
+                    bountyMessage += `${role.id} `
+                })
+                rolesArray.forEach((role) => {
+                    const guildRole = server.roles.find(x => x.name === role)
+                    if (guildRole) {
+                        bountyMessage += `${guildRole.id} `
+                    }
+                })
+                const channel = guild.channels.cache.get(server.earn)
+                if (channel && channel.isTextBased()) {
+                    channel.send(bountyMessage)
+                }
+            }
+        })
     })
 });
 
